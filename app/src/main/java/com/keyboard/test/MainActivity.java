756b12bf5602af8509903160d0cefcb1574bf59b
冯永灿 <fengxiaocan@gmail.com>
2020-03-23 10:44:00 +0800

    兼容屏幕可能改变尺寸以及虚拟导航键显示或隐藏的问题

--- app/src/main/java/com/keyboard/test/MainActivity.java
+++ app/src/main/java/com/keyboard/test/MainActivity.java
@@ -1,10 +1,12 @@
 package com.keyboard.test;
 
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -16,24 +18,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.keyboard.monitor.KeyboardHelper;
 import com.keyboard.monitor.OnKeyboardListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     private TextView tvPan;
     private EditText etInput;
     private Button btAdd;
     private LinearLayout layout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-//        DebugBox.init(getApplication());
-//        DebugBox.get().open();
-
-        KeyboardHelper.registerKeyboardListener(getWindow(), new OnKeyboardListener() {
+    protected void onCreate(Bundle savedInstanceState){
+        //        DebugBox.init(getApplication());
+        //        DebugBox.get().open();
+//        final View decorView = getWindow().getDecorView();
+//        decorView.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener(){
+//            @Override
+//            public void onViewAttachedToWindow(View v){
+//                Log.e("noah","onViewAttachedToWindow=" + v.getClass().getName());
+//            }
+//
+//            @Override
+//            public void onViewDetachedFromWindow(View v){
+//                Log.e("noah","onViewDetachedFromWindow=" + v.getClass().getName());
+//            }
+//        });
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2){
+//            decorView.getViewTreeObserver().addOnWindowAttachListener(new ViewTreeObserver.OnWindowAttachListener(){
+//                @Override
+//                public void onWindowAttached(){
+//                    Log.e("noah","onWindowAttached");
+//                }
+//
+//                @Override
+//                public void onWindowDetached(){
+//                    Log.e("noah","onWindowDetached");
+//                }
+//            });
+//        }
+        KeyboardHelper.registerKeyboardListener(getWindow(),new OnKeyboardListener(){
             @Override
-            public void onKeyBoardEvent(boolean isShow, int height) {
-                if (isShow) {
+            public void onKeyBoardEvent(boolean isShow,int height){
+                if(isShow){
                     tvPan.setVisibility(View.GONE);
-                } else {
-                    if (height > 0) {
+                } else{
+                    if(height > 0){
                         tvPan.setText("键盘高度为:" + height);
                         tvPan.getLayoutParams().height = height;
                         tvPan.setVisibility(View.VISIBLE);
@@ -43,26 +69,29 @@ public class MainActivity extends AppCompatActivity {
         });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        startActivity(new Intent(this,TwoActivity.class));
+
         initView();
-//        Resources system = Resources.getSystem();
-//        TypedValue.applyDimension();
+         Resources system = Resources.getSystem();
+//         TypedValue.applyDimension();
+
         Log.e("noah",getResources().getDisplayMetrics().toString());
+        Log.e("noah", system.getDisplayMetrics().toString());
+        // startActivity(new Intent(this,TwoActivity.class));
     }
 
-    private void initView() {
+    private void initView(){
         tvPan = findViewById(R.id.tv_pan);
         etInput = findViewById(R.id.et_input);
         btAdd = findViewById(R.id.bt_add);
         layout = findViewById(R.id.layout);
-        btAdd.setOnClickListener(new View.OnClickListener() {
+        btAdd.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-//                TextView view = new TextView(MainActivity.this);
-//                view.setText("哈哈");
-//                view.setGravity(View.TEXT_ALIGNMENT_CENTER);
-//                view.setTextColor(Color.BLACK);
-//                layout.addView(view);
+            public void onClick(View v){
+                //                TextView view = new TextView(MainActivity.this);
+                //                view.setText("哈哈");
+                //                view.setGravity(View.TEXT_ALIGNMENT_CENTER);
+                //                view.setTextColor(Color.BLACK);
+                //                layout.addView(view);
                 new AlertDialog.Builder(MainActivity.this).setView(R.layout.layout_input_alert_view).create().show();
             }
         });
--- libs/src/main/java/com/keyboard/monitor/KeyboardHelper.java
+++ libs/src/main/java/com/keyboard/monitor/KeyboardHelper.java
@@ -11,20 +11,20 @@ import android.view.WindowManager;
 import java.util.HashMap;
 import java.util.Map;
 
-public class KeyboardHelper {
+public class KeyboardHelper{
 
     private static final Rect windowRect = new Rect();
-    private static Map<String, ViewTreeObserver.OnGlobalLayoutListener> onLayoutListeners = new HashMap<>();
+    private static Map<String,ViewTreeObserver.OnGlobalLayoutListener> onLayoutListeners = new HashMap<>();
 
-    private static int sKeyboardMinHeight = -1;
-    private static int sKeyboardHeight = -1;
+    private static int sKeyboardMinHeight = - 1;
+    private static int sKeyboardHeight = - 1;
 
     /**
      * 可以自己设置最小高度判断
      *
      * @param sMinHeight
      */
-    public static void initMinHeight(int sMinHeight) {
+    public static void initMinHeight(int sMinHeight){
         KeyboardHelper.sKeyboardMinHeight = sMinHeight;
     }
 
@@ -36,46 +36,46 @@ public class KeyboardHelper {
      * @param isLandscape 是否横屏状态
      * @param listener    回调
      */
-    public static void registerKeyboardListener(Window window, String tag, boolean isLandscape, final OnKeyboardListener listener) {
-        if (window == null || tag == null || listener == null) {
+    public static void registerKeyboardListener(Window window,
+            String tag,
+            boolean isLandscape,
+            final OnKeyboardListener listener)
+    {
+        if(window == null || tag == null || listener == null){
             return;
         }
         //先获取高度
         WindowManager wm = window.getWindowManager();
         wm.getDefaultDisplay().getRectSize(windowRect);
         //判断最小高度
-        if (sKeyboardMinHeight <= 0) {
-            sKeyboardMinHeight = dip2px(window.getContext(), 150);
+        if(sKeyboardMinHeight <= 0){
+            sKeyboardMinHeight = dip2px(window.getContext(),150);
         }
         final int windowRectBottom;
 
-        if (isLandscape) {
+        if(isLandscape){
             windowRectBottom = windowRect.width();
-        } else {
+        } else{
             windowRectBottom = windowRect.bottom;
         }
 
         final View decorView = window.getDecorView();
-        final ViewTreeObserver.OnGlobalLayoutListener layoutListener = new ViewTreeObserver.OnGlobalLayoutListener() {
+        final ViewTreeObserver.OnGlobalLayoutListener layoutListener = new ViewTreeObserver.OnGlobalLayoutListener(){
             @Override
-            public void onGlobalLayout() {
+            public void onGlobalLayout(){
                 decorView.getWindowVisibleDisplayFrame(windowRect);
                 final int dy = windowRectBottom - windowRect.bottom;
-                Log.e("noah",windowRect.toString());
-                if (dy < sKeyboardMinHeight) {
+                if(dy < sKeyboardMinHeight){
                     //键盘没有弹出来
-                    listener.onKeyBoardEvent(false, sKeyboardHeight);
-                } else {
+                    listener.onKeyBoardEvent(false,sKeyboardHeight);
+                } else{
                     //键盘弹出来了
-                    if (sKeyboardHeight <= 0) {
-                        sKeyboardHeight = dy;
-                    }
-//                    Log.e("noah","键盘高度为:"+sKeyboardHeight);
-                    listener.onKeyBoardEvent(true, sKeyboardHeight);
+                    //Log.e("noah","键盘高度为:"+sKeyboardHeight);
+                    listener.onKeyBoardEvent(true,sKeyboardHeight = dy);
                 }
             }
         };
-        onLayoutListeners.put(tag, layoutListener);
+        onLayoutListeners.put(tag,layoutListener);
         decorView.getViewTreeObserver().addOnGlobalLayoutListener(layoutListener);
     }
 
@@ -86,11 +86,11 @@ public class KeyboardHelper {
      * @param isLandscape 是否横屏状态
      * @param listener    回调
      */
-    public static void registerKeyboardListener(Window window, boolean isLandscape, final OnKeyboardListener listener) {
-        if (window == null) {
+    public static void registerKeyboardListener(Window window,boolean isLandscape,final OnKeyboardListener listener){
+        if(window == null){
             return;
         }
-        registerKeyboardListener(window, window.toString(), isLandscape, listener);
+        registerKeyboardListener(window,window.toString(),isLandscape,listener);
     }
 
     /**
@@ -100,8 +100,8 @@ public class KeyboardHelper {
      * @param tag      标记
      * @param listener 回调
      */
-    public static void registerKeyboardListener(Window window, String tag, final OnKeyboardListener listener) {
-        registerKeyboardListener(window, tag, false, listener);
+    public static void registerKeyboardListener(Window window,String tag,final OnKeyboardListener listener){
+        registerKeyboardListener(window,tag,false,listener);
     }
 
     /**
@@ -110,11 +110,11 @@ public class KeyboardHelper {
      * @param window   当前所在的window,可以在dialog中使用
      * @param listener 回调
      */
-    public static void registerKeyboardListener(Window window, final OnKeyboardListener listener) {
-        if (window == null) {
+    public static void registerKeyboardListener(Window window,final OnKeyboardListener listener){
+        if(window == null){
             return;
         }
-        registerKeyboardListener(window, window.toString(), false, listener);
+        registerKeyboardListener(window,window.toString(),false,listener);
     }
 
     /**
@@ -123,12 +123,12 @@ public class KeyboardHelper {
      * @param window 当前所在的window,可以在dialog中使用
      * @param tag    标记
      */
-    public static void unregisterKeyboardListener(Window window, String tag) {
-        if (window == null || tag == null) {
+    public static void unregisterKeyboardListener(Window window,String tag){
+        if(window == null || tag == null){
             return;
         }
-        ViewTreeObserver.OnGlobalLayoutListener listener = onLayoutListeners.get(tag);
-        if (listener != null) {
+        ViewTreeObserver.OnGlobalLayoutListener listener = onLayoutListeners.remove(tag);
+        if(listener != null){
             window.getDecorView().getViewTreeObserver().removeOnGlobalLayoutListener(listener);
         }
     }
@@ -138,18 +138,18 @@ public class KeyboardHelper {
      *
      * @param window 当前所在的window,可以在dialog中使用
      */
-    public static void unregisterKeyboardListener(Window window) {
-        if (window == null) {
+    public static void unregisterKeyboardListener(Window window){
+        if(window == null){
             return;
         }
-        unregisterKeyboardListener(window, window.toString());
+        unregisterKeyboardListener(window,window.toString());
     }
 
     /**
      * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
      */
-    private static int dip2px(Context context, float dpValue) {
+    private static int dip2px(Context context,float dpValue){
         final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
+        return (int)(dpValue * scale + 0.5f);
     }
 }
